d641bbebe4418d744d17859ecf6f6ef89956945c
Elias Dorneles <eliasdorneles@gmail.com>
2014-12-14 23:45:06 -0200

    updating setup script to support linking files to .config/ dir

--- setup-env.sh
+++ setup-env.sh
@@ -5,33 +5,36 @@ absdirname(){
     (cd `dirname $1` 2>/dev/null && pwd || dirname $1)
 }
 
-cria_link(){
-    f="$1"
-    f2=${2:-$1}
-    d=`absdirname $0`
-    if [ -e "$HOME/$f" ]
+create_abs_link(){
+    local target="$1"
+    local origin=${2:-$1}
+    local d=`absdirname $0`
+    if (cd && test -e "$target");
     then
-        echo "~/$f already exists, skipping..."
+        echo "~/$target already exists, skipping..."
     else
-        echo ln -s "$d/$f2" "$f"
-        (cd && ln -s "$d/$f2" "$f")
+        echo ln -s "$d/$origin" "$target"
+        (cd && ln -s "$d/$origin" "$target")
     fi
 }
-cria_dotlink(){
-    cria_link ".$1" "$1"
+
+create_abs_dotlink(){
+    create_abs_link ".$1" "$1"
 }
 
 set -e
 
-cria_dotlink screenrc
-cria_dotlink mplayer
-cria_dotlink gitconfig
-cria_dotlink bashrc
-cria_dotlink bash_aliases
+create_abs_dotlink screenrc
+create_abs_dotlink mplayer
+create_abs_dotlink gitconfig
+create_abs_dotlink bashrc
+create_abs_dotlink bash_aliases
 
+mkdir -p ~/.config
+create_abs_link ~/.config/redshift.conf config/redshift.conf
 
-cria_dotlink vimrc
-cria_dotlink vim
+create_abs_dotlink vimrc
+create_abs_dotlink vim
 if [ ! -d ~/.vim/bundle ]; then
 	mkdir -p ~/.vim/bundle
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
@@ -41,9 +44,13 @@ fi
 
 echo -n "Copying files to ~/bin... "
 mkdir -p ~/bin
-for i in bin/*; do
-    cp "$i" ~/bin
+(
+cd bin;
+for f in *; do
+    target=~/bin/$f
+    cmp --silent $f $target || cp $f $target
 done
+)
 echo done
 
 if [ ! -f ~/bin/zz ]; then
