e1b364f191560cde6955f0e519e7f8442014a1b9
Elias Dorneles <elias.dorneles@hey.com>
2025-10-02 23:12:06 +0200

    cleanup: remove old stuff not needed anymore

--- setup-env.sh
+++ setup-env.sh
@@ -57,35 +57,3 @@ for f in *; do
 done
 )
 echo done
-
-download_file_if_needed(){
-    target_dir="$1"
-    url="$2"
-    filename=$(basename $url)
-
-    mkdir -p $target_dir
-    if ! [ -f $target_dir/$filename ]; then
-        wget $url -O $target_dir/$filename
-    fi
-}
-
-# install powerline fonts and reload if needed
-fonts_dir=~/.fonts
-mkdir -p $fonts_dir
-fontconfig_dir=~/.config/fontconfig/conf.d/
-
-fontlist=$(ls ~/.fonts)
-download_file_if_needed $fonts_dir \
-    https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
-download_file_if_needed $fontconfig_dir \
-    https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-
-if ! diff -q <(echo "$fontlist") <(ls $fonts_dir) >/dev/null; then
-    echo -n "Reloading fonts... "
-    fc-cache -vf $fonts_dir >/tmp/log-fc-cache.log
-    echo "done"
-    echo "You may need to restart X for the new fonts to be used"
-fi
-
-
-pip install --user python-dateutil matplotlib
